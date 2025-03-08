88c0cdfbb66c011b0f3f1ec65c6980ef8255492a
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2025-03-08 00:43:34 -0500

    updates with the front end

---
+++ capital_one_recommender/app/routes/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/questionnaire')
+def questionnaire():
+    return render_template('questionnaire.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+@app.route('/comparison')
+def comparison():
+    return render_template('comparison.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/accessibility')
+def accessibility():
+    return render_template('accessibility.html')
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
